b5fd1e369b26be516798e00cd90aac235755e561
Sai Harshith <saiharshith.poli@gmail.com>
2025-04-30 11:22:52 +0530

    changed prisma generated path

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,12 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
-
-try {
-  prisma = new PrismaClient();
-} catch (error) {
-  console.error("Failed to initialize Prisma Client:", error);
-  process.exit(1);
-}
+const prisma = new PrismaClient();
 
 export default prisma;
